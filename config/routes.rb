c9d3416eff346091c46bc549abc84a70b4323e1d
Roch de Vidts <roch-dv@hotmail.com>
2021-05-27 22:38:16 +0200

    front end watch list, need to finish bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/lists', to: 'lists#index'
+  # get '/lists/new', to: 'lists#new', as: :list_new
+  # post '/lists', to: 'lists#create'
+  # get '/lists/:id', to: 'lists#show', as: :list
+
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
